a712479e01ebd84c82567d22c1ec87129546bb61
Stefan <seike@posteo.de>
2020-03-09 14:31:23 +0100

    Mehr Listen

---
+++ numbers.py
@@ -0,0 +1,13 @@
+# Die Funktion range()
+
+for value in range(1,5):
+    print(value)
+
+for value in range(1,6):
+    print(value)
+
+# Numerische Listen mithilfe von range() aufstellen
+
+numbers = list(range(1,6))
+print(numbers)
+
---
+++ numbers_uebung.py
@@ -0,0 +1,40 @@
+# Aufgabe 4-3
+
+for value in range(1,21):
+    print(value)
+print("\n")
+
+# Aufgabe 4-4
+
+#for value in range(1,1000001):
+#    print(value)
+#print("\n")
+
+# Aufgabe 4-5
+digits = []
+for value in range(1,1000001):
+    digits.append(value)
+print(min(digits))
+print(max(digits))
+
+# Aufgabe 4-6
+uneven_numbers = list(range(1,21,2))
+print(uneven_numbers)
+
+for uneven_numbers in range(1,21,2):
+    print(uneven_numbers)
+
+# Aufgabe 4-7
+
+for value in range (3,31):
+    print(value*3)
+
+# Aufgabe 4-8
+
+for value in range(1,11):
+    print(value**3)
+
+# Aufgabe 4-9
+cubic = [value**3 for value in range(1,11)]
+print(cubic)
+
